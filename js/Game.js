cc828561396c66d09a1961ba4ca1de3ce528a7d9
Altaf Sarker <ahsarker@AHSarker-M07.local>
2020-06-24 18:48:30 -0700

    game

--- js/Game.js
+++ js/Game.js
@@ -30,25 +30,32 @@ class Game {
     }
 
     car1 = createSprite(100,200);
+    car1.addImage("car1", car1img)
     car2 = createSprite(300,200);
+    car2.addImage("car2", car2img)
     car3 = createSprite(500,200);
+    car3.addImage("car3", car3img)
     car4 = createSprite(700,200);
+    car4.addImage("car4", car4img)
     cars = [car1, car2, car3, car4];
   }
 
   play(){
     form.hide();
-
-    Player.getPlayerInfo();
     
+    Player.getPlayerInfo();
+      
+      //image(ground, 0,-displayHeight*5, displayWidth, displayHeight*6);
     if(allPlayers !== undefined){
       //var display_position = 100;
-      
+      background("#D1AE86");
+      image(track, 0, -displayHeight*4, displayWidth, displayHeight*5);
+     
       //index of the array
       var index = 0;
 
       //x and y position of the cars
-      var x = 0;
+      var x = 175;
       var y;
 
       for(var plr in allPlayers){
@@ -78,7 +85,13 @@ class Game {
       player.distance +=10
       player.update();
     }
-
+    if(player.distance>=3700){
+      gameState = 2;
+    }
     drawSprites();
+  }
+  end(){
+    console.log("Game Ended");
+    game.update(2);
   }
 }
