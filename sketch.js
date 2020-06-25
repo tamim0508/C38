a40996149a3bfdc4f71dc37f6a4618ba380d568d
Altaf Sarker <ahsarker@AHSarker-M07.local>
2020-06-24 18:48:39 -0700

    game

--- sketch.js
+++ sketch.js
@@ -9,7 +9,15 @@ var database;
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
-
+var car1img, car2img, car3img, car4img, track
+function preload(){
+car1img = loadImage("images/car1.png");
+car2img = loadImage("images/car2.png");
+car3img = loadImage("images/car3.png");
+car4img = loadImage("images/car4.png");
+track = loadImage("images/track.png");
+ground = loadImage("images/ground.png");
+}
 
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
